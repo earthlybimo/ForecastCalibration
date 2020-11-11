af3f962dc9af5e887681a97575271d4f3ba67741
earhtlybimo <bimochann@gmail.com>
2020-11-11 16:07:45 +0100

    Instead of init 2, we do init 3. But run all the plots

--- TAQM_Method/Plot_Forecasts_withinMistral.R
+++ TAQM_Method/Plot_Forecasts_withinMistral.R
@@ -13,57 +13,57 @@ strtm = c(1, 4, 7, 10)  # which is the starting month for each initialisation
 
 #Which forecast, what time.
 yy=2  #2012
-init=2
+init=3
 mm=1 #lead time month
 for (yy in 1:8) {
   
   
-  ### First for Longjiang's output
-  # loadname=sprintf("%s/F%02d_MEM_ens_mon_mean_corr.nc",data_path,(inYR[yy]-2000))
-  # fl=nc_open(loadname)
-  # longcalSIC=ncvar_get(fl,"SIC_FCST_CORR") # num [1:126858, 1:12, 1:4, 1:30]
-  # nc_close(fl)
-  # longcalSIPo=array(dim=dim(longcalSIC))
-  # longcalSIPo[longcalSIC<0.15]=0
-  # longcalSIPo[longcalSIC>=0.15]=1
-  # longcalSIP=apply(longcalSIPo,c(1,2,3),mean)
-  # 
-  # obsTyr=inYR[yy]
-  # obsTmnth = mm+strtm[init]-1
-  # if (obsTmnth>12){
-  #   obsTyr=obsTyr+1
-  #   obsTmnth=obsTmnth-12}
-  # 
-  # ###print('Targetyear = '+str(targetyear)+',initialisation = '+str(init)+' which means from '+ str(strtm[init-1]) +',leadtime '+str(leadtimeMonth)+' so target month is '+str(obsTmnth)+' of year '+str(obsTyr))  # Testing, python code so need to correct
-  # 
-  # loadname=paste0("/work/ab0995/a270112/data_fesom2/sic/OSISAF_monthly_",obsTyr,".nc")
-  # if(!file.exists(loadname)) next()
-  # fl=nc_open(loadname)
-  # obsVar1=ncvar_get(fl,"obs") # num [1:126858, 1:12]
-  # nc_close(fl)
-  # 
-  # obsVar2=obsVar1[,obsTmnth]
-  # obsSIP=array(dim =length(grd$lat))
-  # obsSIP[obsVar2>=0.15]=1;obsSIP[obsVar2<0.15]=0
-  # 
-  # preSPS=(longcalSIP[,mm,init]-obsSIP)^2   #Diff between model and satelite
-  # preSPS2=preSPS*grd$cell_area
-  # SPScal=sum(preSPS2,na.rm = T)*(10^-12)
-  # 
-  # ## Plot Forecast
-  # pir = sl.plot.init(projection="polar",polar.latbound = 50,file.name = sprintf("%s/LongJSIP_%d_init%d_leadtime%02d.pdf",Figpath,inYR[yy],init,mm))
-  # pcol= sl.plot.field.elem(pir,num=longcalSIP[,mm,init],lon=grd$lon,lat = grd$lat,elem = grd$elem)
-  # res = sl.plot.naturalearth(pir,fill.col = "grey",lines.col = "grey")
-  # res=sl.plot.text(pir,lon=120,lat=65,labels = paste0("SPS = ",SPScal))
-  # sl.plot.lonlatgrid(pir, pole.hole=TRUE,col = "grey",labels = TRUE)
-  # sl.plot.end(pir)
-  # 
-  # ## Plot Obs
-  # pir = sl.plot.init(projection="polar",polar.latbound = 50,file.name = sprintf("%s/ObsSIP_%d_month%02d.pdf",Figpath,obsTyr,obsTmnth))
-  # pcol= sl.plot.field.elem(pir,num=obsSIP,lon=grd$lon,lat = grd$lat,elem = grd$elem)
-  # res = sl.plot.naturalearth(pir,fill.col = "grey",lines.col = "grey")
-  # sl.plot.lonlatgrid(pir, pole.hole=TRUE,col = "grey",labels = TRUE)
-  # sl.plot.end(pir)
+  ## First for Longjiang's output
+  loadname=sprintf("%s/F%02d_MEM_ens_mon_mean_corr.nc",data_path,(inYR[yy]-2000))
+  fl=nc_open(loadname)
+  longcalSIC=ncvar_get(fl,"SIC_FCST_CORR") # num [1:126858, 1:12, 1:4, 1:30]
+  nc_close(fl)
+  longcalSIPo=array(dim=dim(longcalSIC))
+  longcalSIPo[longcalSIC<0.15]=0
+  longcalSIPo[longcalSIC>=0.15]=1
+  longcalSIP=apply(longcalSIPo,c(1,2,3),mean)
+
+  obsTyr=inYR[yy]
+  obsTmnth = mm+strtm[init]-1
+  if (obsTmnth>12){
+    obsTyr=obsTyr+1
+    obsTmnth=obsTmnth-12}
+
+  ###print('Targetyear = '+str(targetyear)+',initialisation = '+str(init)+' which means from '+ str(strtm[init-1]) +',leadtime '+str(leadtimeMonth)+' so target month is '+str(obsTmnth)+' of year '+str(obsTyr))  # Testing, python code so need to correct
+
+  loadname=paste0("/work/ab0995/a270112/data_fesom2/sic/OSISAF_monthly_",obsTyr,".nc")
+  if(!file.exists(loadname)) next()
+  fl=nc_open(loadname)
+  obsVar1=ncvar_get(fl,"obs") # num [1:126858, 1:12]
+  nc_close(fl)
+
+  obsVar2=obsVar1[,obsTmnth]
+  obsSIP=array(dim =length(grd$lat))
+  obsSIP[obsVar2>=0.15]=1;obsSIP[obsVar2<0.15]=0
+
+  preSPS=(longcalSIP[,mm,init]-obsSIP)^2   #Diff between model and satelite
+  preSPS2=preSPS*grd$cell_area
+  SPScal=sum(preSPS2,na.rm = T)*(10^-12)
+
+  ## Plot Forecast
+  pir = sl.plot.init(projection="polar",polar.latbound = 50,file.name = sprintf("%s/LongJSIP_%d_init%d_leadtime%02d.pdf",Figpath,inYR[yy],init,mm))
+  pcol= sl.plot.field.elem(pir,num=longcalSIP[,mm,init],lon=grd$lon,lat = grd$lat,elem = grd$elem)
+  res = sl.plot.naturalearth(pir,fill.col = "grey",lines.col = "grey")
+  res=sl.plot.text(pir,lon=120,lat=65,labels = paste0("SPS = ",SPScal))
+  sl.plot.lonlatgrid(pir, pole.hole=TRUE,col = "grey",labels = TRUE)
+  sl.plot.end(pir)
+
+  ## Plot Obs
+  pir = sl.plot.init(projection="polar",polar.latbound = 50,file.name = sprintf("%s/ObsSIP_%d_month%02d.pdf",Figpath,obsTyr,obsTmnth))
+  pcol= sl.plot.field.elem(pir,num=obsSIP,lon=grd$lon,lat = grd$lat,elem = grd$elem)
+  res = sl.plot.naturalearth(pir,fill.col = "grey",lines.col = "grey")
+  sl.plot.lonlatgrid(pir, pole.hole=TRUE,col = "grey",labels = TRUE)
+  sl.plot.end(pir)
   
   
   ### Let's add the TAQM calibrated forecasts to the lot:
@@ -74,7 +74,7 @@ for (yy in 1:8) {
   fl=nc_open(loadname)
   rawSIP=ncvar_get(fl,"SIP_FCST_RAW")
   calSIP=ncvar_get(fl,"SIP_FCST_CORR")
-  obsSIP=ncvar_get(fl,"SIP_FCST_OBS")
+  # obsSIP=ncvar_get(fl,"SIP_FCST_OBS")
   nc_close(fl)
   
   preSPS=(rawSIP-obsSIP)^2   #Diff between model and satelite
