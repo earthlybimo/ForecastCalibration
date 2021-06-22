0b97c0e289df630f3f56499b4dff8b90a3c9785d
earhtlybimo <bimochann@gmail.com>
2021-06-22 11:51:00 +0200

    Merged scripts so that SPS (or IIEE in the other script) from all forecasts are saved together

--- PostCalbrationChecking/Collect_calibratedIIEE.R
+++ PostCalbrationChecking/Collect_calibratedIIEE.R
@@ -7,7 +7,8 @@ ccc=as.integer(args[1]) #Choice of Hemisphere
 library(ncdf4);library(spheRlab)
 
 save_path = '/work/ba1138/a270138/BiasCorrOutput/TAQMResults'
-
+Clima_path= '/work/ab0995/a270112/data_fesom2/sic'
+SIMPdata_path='/pf/a/a270138/Data/LongjiangOutput/FCST_CLIM'
 
 binarise <-function (somearr,dlevel) {  #Function to binarise some given array  based on  this level
   ll=dim(somearr)
@@ -40,12 +41,23 @@ rawOarr=array(dim=c(length(inYR),4,12))
 rawUarr=rawOarr
 calOarr=rawOarr
 calUarr=rawUarr
+climaOarr=array(dim=c(length(inYR),4,12))
+climaUarr=climaOarr
+simpOarr=climaOarr
+simpUarr=climaUarr
 
 for(yy in 1:length(inYR)){ # yy=1;init=1;mm=1 #For tests
+  loadname=sprintf("%s/F%0.2d_ens_mon_mean_SIP_corr.nc",Data_path,(inYR[yy]-2000))
+  # if(!file.exists(loadname)) next()
+  fl=nc_open(loadname)
+  simpSIP_all=ncvar_get(fl,"SIP_FCST_CORR")
+  nc_close(fl)
+  
   for(init in 1:4){
     for(mm in 1:12){
+      ### Get SIP:
       
-      ## Get Obs
+      # Obs
       obsTyr=inYR[yy]
       obsTmnth = mm+strtm[init]-1
       if (obsTmnth>12){
@@ -64,26 +76,56 @@ for(yy in 1:length(inYR)){ # yy=1;init=1;mm=1 #For tests
       obsSIP[(obsVar2>=0.15)&(obsVar2<=1)]=1
       obsSIP[(obsVar2>=0)&(obsVar2<0.15)]=0
       
+      # TAQM cal forecasts
       loadname=sprintf("%s/Forecast_Calibration_TrustSharpFalse_Yr%d_%02dMn_%02d.nc",save_path,inYR[yy],init,mm)
       if(!file.exists(loadname)) next()
-      
       fl=nc_open(loadname)
       rawSIP=ncvar_get(fl,"SIP_FCST_RAW")
       calSIP=ncvar_get(fl,"SIP_FCST_CORR")
       # obsSIP2=ncvar_get(fl,"SIP_FCST_OBS")
       nc_close(fl)
       
+      ## Climatology:
+      # Let's make our own climatlogy:
+      ClimaSIParr=array(dim =c(9,length(grd$lat)))
+      for(l in 9:1){
+        loadname=paste0("/work/ab0995/a270112/data_fesom2/sic/OSISAF_monthly_",(obsTyr-l),".nc")
+        if(!file.exists(loadname)) {print(paste0("Not found: ",basename(loadname)));next()}
+        fl=nc_open(loadname)
+        obsVar1=ncvar_get(fl,"obs") # num [1:126858, 1:12]
+        nc_close(fl)
+        obsVar2=obsVar1[,obsTmnth]
+        obsSIPtemp=array(dim =length(grd$lat))
+        obsSIPtemp[(obsVar2>=0.15)&(obsVar2<=1)]=1
+        obsSIPtemp[(obsVar2>=0)&(obsVar2<0.15)]=0
+        ClimaSIParr[l,]=obsSIPtemp
+      }
+      climaSIP=colMeans(ClimaSIParr,na.rm = T)
+      
+      ## SIMP cal forecast:
+      simpSIP=simpSIP_all[,mm,init]
+      ## But simp calibration is just calibrated ensemble mean so this is potentially not correct
+      
+      
+      ### Make medians (for IIEE)
+      climaSIPmed=binarise(climaSIP,0.5)
       rawSIPmed=binarise(rawSIP,0.5)
+      calSIPmed=binarise(calSIP,0.5)
+      simpSIPmed=binarise(simpSIP,0.5)
+      
+      
+      ### Measure IIEE (O + U)
+    
+      # raw
       uid=which((rawSIPmed==0)&(obsSIP==1)&(Hemfilter==1)) #identify underforecast grids
       U=sum(grd$cell_area[uid],na.rm=T)  #And sum area
       oid=which((rawSIPmed==1)&(obsSIP==0)&(Hemfilter==1)) #identify overforecast grids
       O=sum(grd$cell_area[uid],na.rm=T)  #And sum area
       rawUarr[yy,init,mm]=U*(10^-12)
       rawOarr[yy,init,mm]=O*(10^-12)
-      
       remove(O,U,uid,oid,rawSIPmed,rawSIP)
       
-      calSIPmed=binarise(calSIP,0.5)
+      # Cal:
       uid=which((calSIPmed==0)&(obsSIP==1)&(Hemfilter==1)) #identify underforecast grids
       U=sum(grd$cell_area[uid],na.rm=T)  #And sum area
       oid=which((calSIPmed==1)&(obsSIP==0)&(Hemfilter==1)) #identify overforecast grids
@@ -93,22 +135,44 @@ for(yy in 1:length(inYR)){ # yy=1;init=1;mm=1 #For tests
       
       remove(O,U,uid,oid,calSIPmed,calSIP)
       
+      # Clima:
+      uid=which((climaSIPmed==0)&(obsSIP==1)&(Hemfilter==1)) #identify underforecast grids
+      U=sum(grd$cell_area[uid],na.rm=T)  #And sum area
+      oid=which((climaSIPmed==1)&(obsSIP==0)&(Hemfilter==1)) #identify overforecast grids
+      O=sum(grd$cell_area[uid],na.rm=T)  #And sum area
+      climaUarr[yy,init,mm]=U*(10^-12)
+      climaOarr[yy,init,mm]=O*(10^-12)
+      remove(O,U,uid,oid,climaSIPmed,climaSIC)
+      
+      #SIMP:
+      uid=which((simpSIPmed==0)&(obsSIP==1)&(Hemfilter==1)) #identify underforecast grids
+      U=sum(grd$cell_area[uid],na.rm=T)  #And sum area
+      oid=which((simpSIPmed==1)&(obsSIP==0)&(Hemfilter==1)) #identify overforecast grids
+      O=sum(grd$cell_area[uid],na.rm=T)  #And sum area
+      simpUarr[yy,init,mm]=U*(10^-12)
+      simpOarr[yy,init,mm]=O*(10^-12)
+      remove(O,U,uid,oid,simpSIPmed,simpSIP)
+      
+      remove(obsSIP)
     }
   }
 }
 
 rawIIEEarr=rawOarr+rawUarr
 calIIEEarr=calOarr+calUarr
+climaIIEEarr=climaOarr+climaUarr
+simpIIEEarr=simpOarr+simpUarr
 Mon_rawIIEE=apply(rawIIEEarr, c(2,3),mean,na.rm=T)
 Mon_calIIEE=apply(calIIEEarr, c(2,3),mean,na.rm=T)
-
+Mon_climaIIEE=apply(climaIIEEarr, c(2,3),mean,na.rm=T)
+Mon_simpIIEE=apply(simpIIEEarr, c(2,3),mean,na.rm=T)
 
 # rawAEEarr=abs(rawOarr- rawUarr )
 # rawMEarr=rawIIEEarr-rawAEEarr
 # calAEEarr=abs(calOarr-calUarr)
 # calMEarr=calIIEEarr-calAEEarr
 
-save(file = Allsavename,version = 2,grd,calIIEEarr,rawIIEEarr,inYR,Mon_rawIIEE,Mon_calIIEE,rawOarr,calOarr)
+save(file = Allsavename,version = 2,grd,calIIEEarr,rawIIEEarr,climaIIEEarr,simpIIEEarr,inYR,Mon_rawIIEE,Mon_calIIEE,Mon_simpIIEE,Mon_climaIIEE,rawOarr,calOarr,simpOarr,climaOarr)
 file.copy(from = Allsavename,to = paste0("~/Data/tomove/",basename(Allsavename)),overwrite = T)
 print(paste0("Done! saved file:",basename(Allsavename)))
 
--- PostCalbrationChecking/Collect_calibratedSPS.R
+++ PostCalbrationChecking/Collect_calibratedSPS.R
@@ -5,7 +5,8 @@ ccc=as.integer(args[1]) #Choice of Hemisphere
 library(ncdf4);library(spheRlab)
 
 save_path = '/work/ba1138/a270138/BiasCorrOutput/TAQMResults'
-
+Clima_path= '/work/ab0995/a270112/data_fesom2/sic'
+SIMPdata_path='/pf/a/a270138/Data/LongjiangOutput/FCST_CLIM'
 
 ##First we need the gridinfo to compute SPS
 grd = sl.grid.readNCDF("/mnt/lustre02/work/ab0995/a270112/data_fesom2/griddes.nc")
@@ -19,7 +20,7 @@ if(ccc == 2)  {
   HEM="sh"
   Hemfilter[grd$lat<0]=1
 }
-Allsavename=paste0(save_path,"/CollectedSPSResult_TrustSharpFalseJune17_",HEM)
+Allsavename=paste0(save_path,"/CollectedSPSResult_AllinclTAQM_",HEM)
 
 
 
@@ -27,10 +28,23 @@ inYR=2011:2018  #which year
 strtm = c(1, 4, 7, 10)  # which is the starting month for each initialisation
 rawSPSarr=array(dim=c(length(inYR),4,12))
 calSPSarr=rawSPSarr
+climaSPSarr=array(dim=c(length(inYR),4,12))
+simpSPSarr=climaSPSarr
 
 for(yy in 1:length(inYR)){
+  
+  loadname=sprintf("%s/F%0.2d_ens_mon_mean_SIP_corr.nc",SIMPdata_path,(inYR[yy]-2000))
+  # if(!file.exists(loadname)) next()
+  fl=nc_open(loadname)
+  SIMPcalSIP_all=ncvar_get(fl,"SIP_FCST_CORR")
+  # This assumes the simple forecasts are stored as SIP in  grid x mm x initialisation for each year
+  nc_close(fl)
+  
   for(init in 1:4){
     for(mm in 1:12){
+      ### Load variables and get SIP:
+      
+      ## TAQM
       loadname=sprintf("%s/Forecast_Calibration_TrustSharpFalse_Yr%d_%02dMn_%02d.nc",save_path,inYR[yy],init,mm)
       if(!file.exists(loadname)) next()
       
@@ -40,7 +54,8 @@ for(yy in 1:length(inYR)){
       # obsSIP=ncvar_get(fl,"SIP_FCST_OBS")
       nc_close(fl)
       
-      #Rather than use the 'pre-saved obsSIP', let's compute it. 
+      
+      ###Obs! Let's compute it:
       obsTyr=inYR[yy]
       obsTmnth = mm+strtm[init]-1
       if (obsTmnth>12){
@@ -59,18 +74,64 @@ for(yy in 1:length(inYR)){
       obsSIP[(obsVar2>=0.15)&(obsVar2<=1)]=1
       obsSIP[(obsVar2>=0)&(obsVar2<0.15)]=0
       
+      ### Climatology:
+      # ClimFile=paste0(Clima_path,"/OSISAF_MON_CLIM_",(obsTyr-9),"-",(obsTyr-1),".nc")
+      # if(!file.exists(ClimFile)) {print(paste0("This Climafile doesnt exist! ",basename(ClimFile))); next()}
+      # fl=nc_open(ClimFile)
+      # climaSIC=ncvar_get(fl,"obs")
+      # nc_close(fl)
+      # climaSIP=binarise(climaSIC[,obsTmnth],0.15)  #This is not clear whether it is climatological SIC, or SIP..
       
+      # Let's make our own climatlogy:
+      ClimaSIParr=array(dim =c(9,length(grd$lat)))
+      for(l in 9:1){
+        loadname=paste0("/work/ab0995/a270112/data_fesom2/sic/OSISAF_monthly_",(obsTyr-l),".nc")
+        if(!file.exists(loadname)) {print(paste0("Not found: ",basename(loadname)));next()}
+        fl=nc_open(loadname)
+        obsVar1=ncvar_get(fl,"obs") # num [1:126858, 1:12]
+        nc_close(fl)
+        obsVar2=obsVar1[,obsTmnth]
+        obsSIPtemp=array(dim =length(grd$lat))
+        obsSIPtemp[(obsVar2>=0.15)&(obsVar2<=1)]=1
+        obsSIPtemp[(obsVar2>=0)&(obsVar2<0.15)]=0
+        ClimaSIParr[l,]=obsSIPtemp
+      }
+      climaSIP=colMeans(ClimaSIParr,na.rm = T)
+      
+      ## SIMP:
+      SIMPcalSIP=SIMPcalSIP_all[,mm,init]
+      ## But simp calibration is just calibrated ensemble mean so not correct
+     
+      
+      ### Compute SPS ### ------------
+      
+      # Clima:
+      preSPS=(climaSIP-obsSIP)^2   #Diff between model and satelite
+      preSPS2=preSPS*grd$cell_area
+      SPSclima=sum(preSPS2[Hemfilter==1],na.rm = T)*(10^-12)
+      climaSPSarr[yy,init,mm]=SPSclima
+      remove(preSPS2,preSPS)
+      
+      # Raw:
       preSPS=(rawSIP-obsSIP)^2   #Diff between model and satelite
       preSPS2=preSPS*grd$cell_area
       SPSraw=sum(preSPS2[Hemfilter==1],na.rm = T)*(10^-12)
       rawSPSarr[yy,init,mm]=SPSraw
       remove(preSPS2,preSPS)
+      
+      # TAQM:
       preSPS=(calSIP-obsSIP)^2   #Diff between model and satelite
       preSPS2=preSPS*grd$cell_area
       SPScal=sum(preSPS2[Hemfilter==1],na.rm = T)*(10^-12)
       calSPSarr[yy,init,mm]=SPScal
       
-      remove(rawSIP,calSIP,obsSIP,SPSraw,SPScal)
+      # SIMP:
+      preSPS=(SIMPcalSIP-obsSIP)^2   #Diff between model and satelite
+      preSPS2=preSPS*grd$cell_area
+      SPScal=sum(preSPS2[Hemfilter==1],na.rm = T)*(10^-12)
+      simpSPSarr[yy,init,mm]=SPScal
+      
+      remove(rawSIP,calSIP,obsSIP,SPSraw,SPScal,climaSIP,SIMPcalSIP)
       
     }
   }
@@ -78,8 +139,11 @@ for(yy in 1:length(inYR)){
 
 Mon_rawSPS=apply(rawSPSarr, c(2,3),mean,na.rm=T)
 Mon_calSPS=apply(calSPSarr, c(2,3),mean,na.rm=T)
+Mon_climaSPS=apply(climaSPSarr, c(2,3),mean,na.rm=T)
+Mon_simpSPS=apply(simpSPSarr, c(2,3),mean,na.rm=T)
 
-save(file = Allsavename,version = 2,grd,calSPSarr,rawSPSarr,inYR,Mon_rawSPS,Mon_calSPS)
+saved_Date=Sys.Date()
+save(file = Allsavename,version = 2,grd,calSPSarr,rawSPSarr,climaSPSarr,simpSPSarr,Mon_rawSPS,Mon_calSPS,Mon_climaSPS,Mon_simpSPS,saved_Date,inYR)
 file.copy(from = Allsavename,to = paste0("~/Data/tomove/",basename(Allsavename)),overwrite = T)
 print(paste0("Done! saved file:",basename(Allsavename)))
 
